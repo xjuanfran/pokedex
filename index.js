4518b39728075180fd5263e3bf6cc2b17c658ddc
xjuanFran <juan.garces.leon@correounivalle.edu.co>
2023-09-03 16:30:47 -0500

    Pokedex v1

---
+++ index.js
@@ -0,0 +1,30 @@
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const inputPokemon = document.getElementById('pokemon'); // Cambiando el nombre de la variable
+const btn = document.getElementById('btn');
+const info = document.getElementById('info');
+
+const insertPokemon = async() =>{
+
+	try {
+		const res = await fetch(`${baseUrl}${pokemon.value.toLowerCase()}`)
+		const pokemonDataJSON = await res.json()
+
+		const pokemonData = {
+			name: pokemonDataJSON.name,
+			img: pokemonDataJSON.sprites.front_default,
+			type: pokemonDataJSON.types,
+			abilities: pokemonDataJSON.abilities
+		}
+        
+        //update de DOM with the pokemon data
+        pokemonName.textContent = pokemonData.name;
+        pokemonImage.src = pokemonData.img;
+        pokemonType.textContent = pokemonData.type.map(type => type.type.name).join(', ');
+        pokemonAbilities.textContent = pokemonData.abilities.map(ability => ability.ability.name).join(', ');
+
+	} catch (error) {
+		alert("That pokemon isn't available. Try againt with another one!");
+	}
+}
+
+btn.addEventListener('click', insertPokemon);
