76f2e191049ec3552b018686a7de1f947a1948a1
xjuanFran <juan.garces.leon@correounivalle.edu.co>
2023-09-05 21:58:05 -0500

    code refactoring with xhr

--- index.js
+++ index.js
@@ -3,29 +3,48 @@ const inputPokemon = document.getElementById('pokemon');
 const btn = document.getElementById('btn');
 const info = document.getElementById('info');
 
-const insertPokemon = async() =>{
+const insertPokemon = async () => {
 
-	try {
-		const res = await fetch(`${baseUrl}${pokemon.value.toLowerCase()}`)
-		const pokemonDataJSON = await res.json()
+  //Implementing with AJAX
+  xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert("That pokemon isn't available. Try againt with another one!");
+    } else {
+      const pokemonData = JSON.parse(xhr.response);
+      pokemonName.textContent = pokemonData.name;
+      pokemonImage.src = pokemonData.sprites.front_default;
+      pokemonImage.alt = pokemonData.name;
+      pokemonType.textContent = pokemonData.types.map(type => type.type.name).join(', ');
+      pokemonAbilities.textContent = pokemonData.abilities.map(ability => ability.ability.name).join(', ');
+    }
+    //Implementing the same with fetch :)
+    /**
+     * try{
+    const res = await fetch(`${baseUrl}${pokemon.value.toLowerCase()}`)
+    const pokemonDataJSON = await res.json()
 
-		const pokemonData = {
-			name: pokemonDataJSON.name,
-			img: pokemonDataJSON.sprites.front_default,
-			type: pokemonDataJSON.types,
-			abilities: pokemonDataJSON.abilities
-		}
+    const pokemonData = {
+      name: pokemonDataJSON.name,
+      img: pokemonDataJSON.sprites.front_default,
+      type: pokemonDataJSON.types,
+      abilities: pokemonDataJSON.abilities
+    }
 
-        //update de DOM with the pokemon data
-        pokemonName.textContent = pokemonData.name;
-        pokemonImage.src = pokemonData.img;
-        pokemonImage.alt = pokemonData.name;
-        pokemonType.textContent = pokemonData.type.map(type => type.type.name).join(', ');
-        pokemonAbilities.textContent = pokemonData.abilities.map(ability => ability.ability.name).join(', ');
+    //update de DOM with the pokemon data
+    pokemonName.textContent = pokemonData.name;
+    pokemonImage.src = pokemonData.img;
+    pokemonImage.alt = pokemonData.name;
+    pokemonType.textContent = pokemonData.type.map(type => type.type.name).join(', ');
+    pokemonAbilities.textContent = pokemonData.abilities.map(ability => ability.ability.name).join(', ');
+    catch(err){
+      alert("That pokemon isn't available. Try againt with another one!");
+    }
+    **/
 
-	} catch (error) {
-		alert("That pokemon isn't available. Try againt with another one!");
-	}
+  }
+  xhr.open('GET', `${baseUrl}${inputPokemon.value.toLowerCase()}`);
+  xhr.send();
 }
 
 btn.addEventListener('click', insertPokemon);
